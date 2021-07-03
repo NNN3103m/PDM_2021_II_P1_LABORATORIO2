08d64d5ba9070520b8f129272bd28f0d054df31d
Daniel Alvarez <70470536+djag07907@users.noreply.github.com>
2021-07-02 19:15:18 -0600

    Btn handler integration

--- app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p1_laboratorio2/RealizarMatricula.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p1_laboratorio2/RealizarMatricula.kt
@@ -2,10 +2,16 @@ package hn.edu.ujcv.pdm_2021_ii_p1_laboratorio2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_realizar_matricula.*
+import kotlinx.android.synthetic.main.activity_registrar_alumno.*
 
 class RealizarMatricula : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_realizar_matricula)
+
+        //        Btn Handler
+        btnOnBack3.setOnClickListener{onBackPressed()}
+        btnSaveEnroll.setOnClickListener{}
     }
 }
--- app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p1_laboratorio2/RegistrarAlumno.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p1_laboratorio2/RegistrarAlumno.kt
@@ -2,13 +2,16 @@ package hn.edu.ujcv.pdm_2021_ii_p1_laboratorio2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_registrar_alumno.*
 
 class RegistrarAlumno : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrar_alumno)
 
-
+//        Btn Handler
+        btnOnBack2.setOnClickListener{onBackPressed()}
+        btnSaveStnt.setOnClickListener{}
 
     }
 }
