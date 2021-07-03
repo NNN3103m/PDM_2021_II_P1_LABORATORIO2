87fa423113bf5d5f0e6d4cdb20244c2a3b694495
Daniel Alvarez <70470536+djag07907@users.noreply.github.com>
2021-07-02 19:09:38 -0600

    Frontend init design

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p1_laboratorio2/RegistrarAlumno.kt
@@ -0,0 +1,14 @@
+package hn.edu.ujcv.pdm_2021_ii_p1_laboratorio2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrarAlumno : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrar_alumno)
+
+
+
+    }
+}
